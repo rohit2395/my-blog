91882cbfea575690a0359ad512c892a8ad97dc1b
Rajbanshi <Rohit.Rajbanshi@emc.com>
2020-03-29 12:31:53 +0530

    Initial commit with database connection and user and post tables created

---
+++ myblog/src/main/java/com/rohit/myblog/model/Constants.java
@@ -0,0 +1,12 @@
+package com.rohit.myblog.model;
+
+
+public interface Constants {
+	
+	//Database constants
+	String SCHEMA = "myblog";
+	String POST_TABLE = "POST";
+	String USER_TABLE = "USER";
+	
+
+}
